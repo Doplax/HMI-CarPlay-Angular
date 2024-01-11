fd62cf08dcfaca89a73b0a8c3ec3cbb9de60b2ce
Pol Valle (Doplax) <doplax@gmail.com>
2024-01-11 13:53:25 +0100

    chore: prepare project for testing phase
    This commit sets up the project in a 'factory' state, ensuring it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
-import { DisplayComponent } from './components/display/display.component';
+//import { SideBarComponent } from './components/side-bar/side-bar.component';
+//import { DisplayComponent } from './components/display/display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent,
-    DisplayComponent
+    //SideBarComponent,
+    //DisplayComponent
   ],
   imports: [
     BrowserModule,
