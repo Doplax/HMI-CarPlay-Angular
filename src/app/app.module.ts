c2977b053b69767d917f1c29c3db27e1b4b9eb80
Pol Valle (Doplax) <Pol.valle@vasscompany.com>
2024-03-12 14:55:05 +0100

    ng-content: Uso de de directivas para reordenar los elementos en un ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 //import { SideBarComponent } from './components/side-bar/side-bar.component';
-//import { DisplayComponent } from './components/display/display.component';
+import { DisplayComponent } from './components/display/display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     //SideBarComponent,
-    //DisplayComponent
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
