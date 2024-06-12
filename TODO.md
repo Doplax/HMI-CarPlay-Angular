577fbf1026acd0955695fb1e92b39ca290ed9315
Pol Valle (Doplax) <doplax@gmail.com>
2024-06-12 14:42:10 +0200

    chore: create basic WebApps modules

--- TODO.md
+++ TODO.md
@@ -6,3 +6,6 @@
 
 
 [] "Desktop First" View so that in case it is viewed on mobile it will warn that due to the size and requirements of the project, it is designed to be viewed on a desktop screen.
+
+
+[] does it make sense to handle routes using svg/icons.json?
---
+++ src/app/modules/books/books.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BooksPageComponent } from './pages/books-page/books-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    BooksPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BooksModule { }
