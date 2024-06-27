915541cf0db1f1b7ef81a1eb0d4b9ebc85ab6d41
Pol Valle (Doplax) <doplax@gmail.com>
2024-06-27 16:02:51 +0200

    feat: implements volume-box connectivity with its service

--- src/app/shared/components/volume-box/volume-box.component.ts
+++ src/app/shared/components/volume-box/volume-box.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CurrentStateService } from '../../services/current-state.service';
 
 @Component({
   selector: 'app-volume-box',
   templateUrl: './volume-box.component.html',
   styleUrls: ['./volume-box.component.scss']
 })
-export class VolumeBoxComponent {
+export class VolumeBoxComponent implements OnDestroy {
+
+// Todo: is the better way to subscribe and unsubscribe? or passing props from the parent
+  public volumeLevel!: number;
+  private subscriptions: Subscription = new Subscription();
+
+
+
+  constructor(private currentStateService: CurrentStateService){
+
+    this.subscriptions.add(
+      this.currentStateService.getVolumeLevel().subscribe(status => {
+        this.volumeLevel = status;
+        console.log(this.volumeLevel);
+      })
+    );
+  }
+   ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+
 
 }
--- src/app/shared/services/current-state.service.ts
+++ src/app/shared/services/current-state.service.ts
@@ -5,7 +5,8 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class CurrentStateService {
-  public isRunning$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true); // Valor inicial
+  public isRunning$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true); // Valor inicial para lockScreen
+  public volumeLevel$: BehaviorSubject<number> = new BehaviorSubject<number>(50);
 
   constructor() { }
 
@@ -15,6 +16,28 @@ export class CurrentStateService {
 
   getRunningState() {
     return this.isRunning$.asObservable();
+  }
+
+  turnUpVolume() {
+    let currentVolumeLevel = this.volumeLevel$.value
+
+    if (currentVolumeLevel < 100) {
+      const newVolumeLevel =  this.volumeLevel$.value + 10
+      this.volumeLevel$.next(newVolumeLevel);
+    }
+  }
+
+  turnDownVolume() {
+    let currentVolumeLevel = this.volumeLevel$.value
+
+    if (currentVolumeLevel > 0) {
+      const newVolumeLevel =  this.volumeLevel$.value - 10
+      this.volumeLevel$.next(newVolumeLevel);
+    }
+  }
+
+  getVolumeLevel() {
+    return this.volumeLevel$.asObservable();
   }
 
 }
