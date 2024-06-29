35c51ae0aad20a76e301863982f13aaeed6fb548
Doplax <doplax@gmail.com>
2024-06-29 13:52:56 +0200

    refactor(volume-service): simplify code, comments and documentation

--- src/app/shared/services/volume.service.ts
+++ src/app/shared/services/volume.service.ts
@@ -5,88 +5,51 @@ import { BehaviorSubject, Subscription, timer } from 'rxjs';
   providedIn: 'root',
 })
 export class VolumeService {
-  public volumeLevel$: BehaviorSubject<number> = new BehaviorSubject<number>(
-    50
-  );
-  public isVolumeVisible$: BehaviorSubject<boolean> =
-    new BehaviorSubject<boolean>(false);
-  private visibilityTimerSubscription: Subscription | null = null; // Mantiene la suscripción al temporizador
+  public volumeLevel$: BehaviorSubject<number> = new BehaviorSubject<number>(50);
+  public isVolumeVisible$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private visibilityTimerSubscription: Subscription | null = null;  // Tracks timer subscription
 
   constructor() {}
 
+  // Manages visibility state of the volume control.
   private triggerVisibility() {
-    if (this.isVolumeVisible$.value === true) {
-      // Si ya está visible, reiniciar el temporizador sin hacer nada más
-      this.resetVisibilityTimer();
-    } else {
-      // Si no está visible, hacerlo visible y configurar el temporizador
+    if (!this.isVolumeVisible$.value) {
       this.isVolumeVisible$.next(true);
-      this.resetVisibilityTimer();
     }
+    this.resetVisibilityTimer();
   }
+
+  // Resets the visibility timer to hide volume control after 4 seconds.
   private resetVisibilityTimer() {
-    // Cancelar el temporizador actual si existe
-    if (this.visibilityTimerSubscription) {
-      this.visibilityTimerSubscription.unsubscribe();
-    }
-    // Crear un nuevo temporizador
-    this.visibilityTimerSubscription = timer(3000).subscribe(() => {
+    this.visibilityTimerSubscription?.unsubscribe();
+    this.visibilityTimerSubscription = timer(4000).subscribe(() => {
       this.isVolumeVisible$.next(false);
     });
   }
 
-  /** TODO: Review this method
-   * Gradually changes the volume level, either increasing or decreasing it
-   * by 10 units with smooth increments of 1 unit every 25 milliseconds.
-   *
-   * @param {boolean} isIncreasing - Indicates the direction of the volume change.
-   *  - `true` to increase the volume.
-   *  - `false` to decrease the volume.
-   *
-   * The function uses `setInterval` to change the volume by one unit every 25 milliseconds
-   * until the target is reached, which is 10 units more or less than the current volume,
-   * without exceeding the limits of 0 to 100.
-   *
-   * Usage example:
-   * - `changeVolume(true)` will increase the volume.
-   * - `changeVolume(false)` will decrease the volume.
+  /**
+   * Changes volume by +/-10 with increments of 1 every 25ms.
+   * @param isIncreasing - `true` to increase, `false` to decrease.
    */
   changeVolume(isIncreasing: boolean) {
     this.triggerVisibility();
 
     const currentVolumeLevel = this.volumeLevel$.value;
     const incrementAmount = 1;
-    const intervalTime = 25; // Adjusted for a smoother increment
+    const intervalTime = 25;
+    const targetVolumeLevel = isIncreasing
+      ? Math.min(currentVolumeLevel + 10, 100)
+      : Math.max(currentVolumeLevel - 10, 0);
 
-    if (isIncreasing) {
-      if (currentVolumeLevel < 100) {
-        const targetVolumeLevel = Math.min(currentVolumeLevel + 10, 100);
-
-        const interval = setInterval(() => {
-          const updatedVolumeLevel = this.volumeLevel$.value;
-
-          if (updatedVolumeLevel < targetVolumeLevel) {
-            this.volumeLevel$.next(updatedVolumeLevel + incrementAmount);
-          } else {
-            clearInterval(interval);
-          }
-        }, intervalTime);
+    const interval = setInterval(() => {
+      const updatedVolumeLevel = this.volumeLevel$.value;
+      if ((isIncreasing && updatedVolumeLevel < targetVolumeLevel) ||
+          (!isIncreasing && updatedVolumeLevel > targetVolumeLevel)) {
+        this.volumeLevel$.next(updatedVolumeLevel + (isIncreasing ? incrementAmount : -incrementAmount));
+      } else {
+        clearInterval(interval);
       }
-    } else {
-      if (currentVolumeLevel > 0) {
-        const targetVolumeLevel = Math.max(currentVolumeLevel - 10, 0);
-
-        const interval = setInterval(() => {
-          const updatedVolumeLevel = this.volumeLevel$.value;
-
-          if (updatedVolumeLevel > targetVolumeLevel) {
-            this.volumeLevel$.next(updatedVolumeLevel - incrementAmount);
-          } else {
-            clearInterval(interval);
-          }
-        }, intervalTime);
-      }
-    }
+    }, intervalTime);
   }
 
   getVolumeLevel() {
