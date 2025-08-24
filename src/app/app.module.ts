02b1fa3bb020406e84a0f5a5cf5b29c356483a71
Doplax <doplax@gmail.com>
2025-08-24 21:10:01 +0200

    chore: update to Angular v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { LockScreenComponent } from './shared/components/lock-screen/lock-screen.component';
 import { VolumeBoxComponent } from './shared/components/volume-box/volume-box.component';
 import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
 
-@NgModule({
-  declarations: [AppComponent, SideBarComponent, LockScreenComponent, VolumeBoxComponent, TopBarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, SideBarComponent, LockScreenComponent, VolumeBoxComponent, TopBarComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
