f7490977fa971ad3c61c6382f1a57ca9b0a43361
Doplax <doplax@gmail.com>
2025-08-24 21:01:57 +0200

    refactor(shared): move icon service to shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { SideBarComponent } from './shared/components/side-bar/side-bar.componen
 import { HttpClientModule } from '@angular/common/http';
 import { LockScreenComponent } from './shared/components/lock-screen/lock-screen.component';
 import { VolumeBoxComponent } from './shared/components/volume-box/volume-box.component';
+import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, SideBarComponent, LockScreenComponent, VolumeBoxComponent],
+  declarations: [AppComponent, SideBarComponent, LockScreenComponent, VolumeBoxComponent, TopBarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
