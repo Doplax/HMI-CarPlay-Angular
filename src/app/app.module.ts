d3cfc57c5d9a88ed20a706d2cd1618acbd734384
Doplax <doplax@gmail.com>
2025-09-18 09:29:51 +0200

    feat(maps) create maps-navigation-controllers and maps-search-box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { LockScreenComponent } from './shared/components/lock-screen/lock-screen.component';
-import { VolumeBoxComponent } from './shared/components/volume-box/volume-box.component';
-import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
-@NgModule({ declarations: [AppComponent, SideBarComponent, LockScreenComponent, VolumeBoxComponent, TopBarComponent],
-    bootstrap: [AppComponent], imports: [BrowserModule, AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+import { LockScreenComponent } from '@shared/components/lock-screen/lock-screen.component';
+import { VolumeBoxComponent } from '@shared/components/volume-box/volume-box.component';
+import { TopBarComponent } from '@shared/components/top-bar/top-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SideBarComponent,
+    LockScreenComponent,
+    VolumeBoxComponent,
+    TopBarComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
 export class AppModule {}
---
+++ src/app/modules/maps/components/maps-search-box/maps-search-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maps-search-box',
+  standalone: false,
+  templateUrl: './maps-search-box.component.html',
+  styleUrl: './maps-search-box.component.scss'
+})
+export class MapsSearchBoxComponent {
+
+}
